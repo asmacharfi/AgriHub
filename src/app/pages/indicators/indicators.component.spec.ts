8f48e69dfd0a869be04283035036ee8d68713a30
Charfi AsMa <acharfi580@gmail.com>
2024-03-14 14:58:15 +0100

    new component name indicators

---
+++ src/app/pages/indicators/indicators.component.spec.ts
@@ -0,0 +1,13 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IndicatorsComponent } from './indicators.component';
+
+describe('DashboardComponent', () => {
+  let component: IndicatorsComponent;
+  let fixture: ComponentFixture<IndicatorsComponent>;
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
