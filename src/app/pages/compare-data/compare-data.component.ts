b10be9ff1c6d63d9ea61d1800826a9bba2ecbef5
Asma <acharfi580@gmail.com>
2024-03-27 10:09:20 +0100

    compare-data design

--- src/app/pages/compare-data/compare-data.component.ts
+++ src/app/pages/compare-data/compare-data.component.ts
@@ -7,20 +7,20 @@ import { Component } from '@angular/core';
 export class CompareComponent {
   startYear: number;
   endYear: number;
-  filters: any[] = []; // Array to hold filter containers
-  // Method to add a new filter container
+  filters: any[] = [];
+  
+  
   addFilter() {
-    this.filters.push({}); // Push an empty object to the filters array
+    this.filters.push({}); 
   }
   removeFilter(index: number) {
-    this.filters.splice(index, 1); // Supprimer le filtre à l'index donné
+    this.filters.splice(index, 1);
   }
   
   groups: Group[] = [
     {
       name: 'Land, Inputs and Sustainability',
       domains: [
-        // Populate this with the relevant domains and areas for this group
         { name: 'Domain 1', areas: ['Area 1.1', 'Area 1.2'] },
         { name: 'Domain 2', areas: ['Area 2.1', 'Area 2.2'] }
       ]
@@ -28,14 +28,12 @@ export class CompareComponent {
     {
       name: 'Forestry',
       domains: [
-        // Populate this with the relevant domains and areas for this group
         { name: 'Forest Domain 1', areas: ['Forest Area 1.1', 'Forest Area 1.2'] }
       ]
     },
     {
       name: 'Climate Change: Agrifood systems emissions',
       domains: [
-        // Populate this with the relevant domains and areas for this group
         { name: 'Emissions Domain 1', areas: ['Emission Area 1.1', 'Emission Area 1.2'] }
       ]
     }
@@ -43,14 +41,11 @@ export class CompareComponent {
   selectedGroup: Group | null = null;
   selectedDomain: Domain | null = null;
   onGroupChange() {
-    this.selectedDomain = null; // Reset domain when group changes
+    this.selectedDomain = null; 
   }
   onDomainChange() {
-    // Handle logic when domain changes, if necessary
   }
-  // Fonction pour lancer la comparaison des données
   compareData() {
-    // Implémenter la logique de comparaison
   }
 }
 export interface Group {
